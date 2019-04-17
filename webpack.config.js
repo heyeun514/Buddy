344cd03d96b8aeecb43e1a8eae5cf73e511f67ee
hyeeun <heyeun514@gmail.com>
2019-04-17 22:55:34 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+
+module.exports = {
+    entry: [
+      './src/index.js',
+      './src/index.css'
+    ],
+    output: {
+      path: __dirname,
+      publicPath: '/',
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "script-loader"
+          }
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        }
+      ]
+    }
+  };
